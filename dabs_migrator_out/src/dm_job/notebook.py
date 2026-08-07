f150c414030590c8027a62abf3c91e59ec7cc49c
stulib <cs_libby@hotmail.com>
2026-08-07 18:31:15 +0000

    deleted resources

--- dabs_migrator_out/src/dm_job/notebook.py
+++
@@ -1,9 +0,0 @@
-# Databricks notebook source
-# Seed notebook for the dabs-migrator test job.
-print("dm_job seed notebook running")
-
-# COMMAND ----------
-
-df = spark.sql("SELECT 1 AS one, 'seed' AS label")
-df.show()
-
--- dabs_migrator_out/src/dm_pipeline/bronze.py
+++
@@ -1,8 +0,0 @@
-# Databricks notebook source
-import dlt
-
-
-@dlt.table(comment="Seed bronze table for the dabs-migrator test pipeline")
-def dm_bronze():
-    return spark.range(10).withColumnRenamed("id", "row_id")
-
